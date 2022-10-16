fef3084c7634d452c4fc53427926324c93227925
Sabika Ali <Sabikaalghadeeri@gmail.com>
2022-10-16 10:16:37 +0300

    my favorite animals are complete

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const express = require('express')
 const app = express()
 const PORT = 3000
+const ejsLayouts = require('express-ejs-layouts')
 
 // MIDDLEWARE
 app.set('view engine','ejs')
+app.use(ejsLayouts)
 
 // ROUTES
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+// favorite animals
+app.get('/animals', (req, res)=>{
+    res.render('animals.ejs', {animals: ['sand crab', 'corny joke dog', 'benedict the sea cucumber']})
+})
+
 app.listen (PORT, ()=>{
-    console.log("LAYOUTS AND CONTROLLERS")
+    console.log(`You're running the aheba-akerha app!`)
 })
